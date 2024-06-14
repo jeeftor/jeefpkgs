3f9382c9683291a7b21122fdb2ff7d143fbe7b04
Jeef <jeeftor@users.noreply.github.com>
2024-06-14 09:40:37 -0600

    adding flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Overlay for custom NixOS configurations";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      overlay = import ./overlay.nix;
+    in {
+      overlays = [ overlay ];
+
+      # Provide the overlay as a package
+      packages.x86_64-linux = nixpkgs.lib.mkShell {
+        inherit overlay;
+      };
+    };
+}
